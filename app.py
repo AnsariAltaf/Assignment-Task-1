fb24deddb7c7426a7b2407703c839c4ff8c0134a
AnsariAltaf <ansarialtaf23.aa@gmail.com>
2022-11-12 18:46:20 +0530

    Second commit for practice

--- app.py
+++ app.py
@@ -4,3 +4,22 @@ x=15
 y=6
 z=x+y
 print("The value of z is " + z)
+from datetime import datetime
+
+
+def get_value(self):
+    pass
+
+def start_data_ingestion(self):
+    pass
+
+def start_data_validation(self):
+    pass
+
+def start_data_training(self):
+    pass
+ 
+def start_data_modelling(self):
+    pass
+
+
